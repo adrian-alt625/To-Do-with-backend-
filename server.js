5ffc1c77dfd6ddc6445306cf944ebc1dcb88b927
adrian-alt625 <adriandinkov625@gmail.com>
2025-02-16 08:31:53 +0000

    Initial Commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 2000;
+//
+/** write code here */
+//
+app.listen(
+  port,
+  console.log("✅ server has started on http://localhost:" + port)
+);
